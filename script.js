66ee3722f9776675c4b4d698112578f490e7c831
Fakhreddine85 <64703898+Fakhreddine85@users.noreply.github.com>
2021-09-16 21:43:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,9 @@ function compute()
     var interest = principal*years*rate/100;
     var year = new Date().getFullYear()+parseInt(years);
     var textresult =  'if you deposit <mark>' +principal+ ' </mark><br> at an interest rate of <mark>' +rate+ '</mark><br> you will receive an amout of <mark>' + interest+ '</mark><br> in the year '  + year+'</div>';
-    //var textresult = '<div id="gator">Alligators rule!!</div>'
+    
     document.getElementById("result").innerHTML  = textresult;
-    // console.log(principal);
-    // console.log(rate);
-    // console.log(years);
+    
     }
     
     
